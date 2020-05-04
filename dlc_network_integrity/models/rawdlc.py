f41272fae5dd964127cfd0b120278a9a4e92a6d8
Krista <kristakernodle@gmail.com>
2020-05-04 15:54:36 +0000

    First commit

---
+++ dlc_network_integrity/models/rawdlc.py
@@ -0,0 +1,23 @@
+#from iteration_utilities import first
+import pandas as pd
+
+
+class Rawdlc:
+    def __init__(self, filepath):
+        with open(filepath, 'rb') as f:
+            data = pd.read_hdf(filepath)
+        network = data.columns[0][0]
+        bodyparts = set()
+        for col in data.columns:
+            bodypart = col[1]
+            bodyparts.add(bodypart)
+
+        self.filepath = filepath
+        self.network = network
+        self.bodyparts = bodyparts
+        self.data = data[network]
+
+    def get_loc(self, bodypart):
+        return self.data[bodypart]
+        
+
